c3533d2366ffd650d7281edd252a17afe50ea1e4
David Kurniawan <david@longblackcode.com>
2019-07-23 14:13:26 +1200

    added cart items in cart dropdown

--- src/components/cart/dropdown/index.jsx
+++ src/components/cart/dropdown/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './index.scss';
+import { connect } from 'react-redux';
 import InputButton from '../../input-button';
+import CartItem from '../item';
 
-const CartDropdown = () => (
+const CartDropdown = ({ items }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {items.map(item => (
+        <CartItem key={item.id} item={item} />
+      ))}
+    </div>
     <InputButton>Check Out</InputButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { items } }) => ({ items });
+
+export default connect(mapStateToProps)(CartDropdown);
